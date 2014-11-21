6a9952629c415afb6f967e086b49c86bf38163ee
Andy Stanton <andystanton@gmail.com>
2014-11-21 16:08:18 +0000

    add classic recursion java solution for problem 2

---
+++ 2/java/ClassicRecursion.java
@@ -0,0 +1,18 @@
+class ClassicRecursion {
+    private static final int MAX = 4000000;
+
+    private static int sumFib(int first, int second) {
+        int next = first + second;
+        int base;
+        if (next < MAX) {
+            base = sumFib(second, next);
+        } else {
+            base = (next % 2 == 0) ? next : 0;
+        }
+        return base + (second % 2 == 0 ? second : 0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumFib(1, 2));
+    }
+}
