54886968959ad4e37ebd44de69a44eec204da05a
Andy Stanton <andystanton@gmail.com>
2014-11-21 11:26:24 +0000

    name java stuff appropriately

--- 1/java/RangeStream.java
+++ 1/java/RangeStream.java
@@ -1,6 +1,6 @@
 import java.util.stream.*;
 
-public class One {
+public class RangeStream {
     public static void main(String[] args) {
         int result = IntStream.range(1, 1000).filter(i -> {
             return i % 3 == 0 || i % 5 == 0;
