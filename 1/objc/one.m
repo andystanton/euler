d27ba084441e72a5852b843f49f45a4a1ff47514
Andy Stanton <andystanton@gmail.com>
2014-11-22 02:05:16 +0000

    add objective c

--- 1/objc/one.m
+++ 1/objc/one.m
@@ -2,9 +2,16 @@
 
 int main (int argc, const char * argv[])
 {
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int sum = 0;
+    int i;
+    for (i = 0; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
 
-    printf ("...\n");
-    [pool drain];
+    printf ("%i\n", sum);
     return 0;
 }
