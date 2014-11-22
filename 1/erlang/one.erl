f86c40597f0995690aae2b15fff91d159bafc574
Andy Stanton <andystanton@gmail.com>
2014-11-22 16:42:26 +0000

    add (bad) erlang

--- 1/erlang/one.erl
+++ 1/erlang/one.erl
@@ -1,5 +1,17 @@
 -module(one).
--export([main/0]).
+-export([main/0, sum/1, sum/2, mod/2, addIfDivisibleBy3or5/2]).
 
 main() ->
-    io:fwrite("...\n").
+    io:format("~p\n", [sum(lists:seq(1,999))]).
+
+sum(L) -> sum(L,0).
+sum([], Sum) -> Sum;
+sum([H|T], Sum) -> sum(T, addIfDivisibleBy3or5(Sum, H)).
+
+mod(X,Y)->(X rem Y + Y) rem Y.
+
+addIfDivisibleBy3or5(Sum, H) ->
+    case (mod(H, 3) == 0) or (mod(H, 5) == 0) of
+        true -> H+Sum;
+        _Else -> Sum
+    end.
