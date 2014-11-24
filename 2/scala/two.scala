2c97bc525bd23d704e3e5d2cb91a0f344f253342
Andy Stanton <andystanton@gmail.com>
2014-11-24 09:29:16 +0000

    no point reversing fib sequence

--- 2/scala/two.scala
+++ 2/scala/two.scala
@@ -1,16 +1,14 @@
 import annotation.tailrec
 
-def fibs(max: Int): Seq[Int] = {
-  fibs(Seq(2, 1), max)
+def rfibs(max: Int): Seq[Int] = {
+  rfibs(Seq(2, 1), max)
 }
 
-@tailrec def fibs(fibSeq: Seq[Int], max: Int): Seq[Int] = {
+@tailrec def rfibs(fibSeq: Seq[Int], max: Int): Seq[Int] = {
   fibSeq.head + fibSeq.tail.head match {
-    case valid: Int if valid < max => fibs(Seq(valid) ++ fibSeq, max)
-    case _ => fibSeq.reverse
+    case valid: Int if valid < max => rfibs(Seq(valid) ++ fibSeq, max)
+    case _ => fibSeq
   }
 }
 
-val fibsTo4Million = fibs(4000000)
-
-println(fibsTo4Million.filter(_ % 2 == 0).sum)
+println(rfibs(4000000).filter(_ % 2 == 0).sum)
