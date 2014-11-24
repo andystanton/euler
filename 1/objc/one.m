ce0bbd6cad1f9a498c85c5c442174b96a4a6c4bb
Andy Stanton <andystanton@gmail.com>
2014-11-24 18:16:34 +0000

    brevity

--- 1/objc/one.m
+++ 1/objc/one.m
@@ -2,8 +2,7 @@
 
 int main (int argc, const char * argv[])
 {
-    int sum = 0;
-    int i;
+    int sum = 0, i;
     for (i = 0; i < 1000; i++)
     {
         if (i % 3 == 0 || i % 5 == 0)
--- 1/ruby/one.rb
+++ 1/ruby/one.rb
@@ -1,5 +1,1 @@
-#(1..9).to_a.keep_if{|i| i % 3 == 0 || i % 5 == 0}.each{|i| puts i}
-sum=0
-(1..999).to_a.keep_if{|i| i % 3 == 0 || i % 5 == 0}.each{|i| sum += i}
-
-puts sum
+puts (1..999).to_a.keep_if{|i| i % 3 == 0 || i % 5 == 0}.inject{|sum,x| sum + x }
