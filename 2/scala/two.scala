5b80a0c0c6bcd6743f8f478a6c08ebb040ac966e
Andy Stanton <andystanton@gmail.com>
2014-11-24 10:26:50 +0000

    brevity

--- 2/scala/two.scala
+++ 2/scala/two.scala
@@ -1,12 +1,10 @@
-def rfibs(max: Int): Seq[Int] = {
-  rfibs(Seq(2, 1), max)
-}
-
 @annotation.tailrec def rfibs(fibs: Seq[Int], max: Int): Seq[Int] = {
   fibs.head + fibs.tail.head match {
     case next: Int if next <= max => rfibs(Seq(next) ++ fibs, max)
     case _ => fibs
   }
 }
+
+def rfibs(max: Int): Seq[Int] = rfibs(Seq(2, 1), max)
 
 println(rfibs(4000000).filter(_ % 2 == 0).sum)
