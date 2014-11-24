aa23ee9976b7fabbf5632c641458eb67714463a3
Andy Stanton <andystanton@gmail.com>
2014-11-24 18:20:15 +0000

    fix node vars

--- 1/node/reduce.js
+++ 1/node/reduce.js
@@ -2,8 +2,8 @@ function range(max) {
     return Array.apply(null, Array(max)).map(function (_, i) {return i;});
 }
 
-var sumIfDivisibleBy3Or5 = range(1000).reduce(function(previousValue, currentValue) {
+var result = range(1000).reduce(function(previousValue, currentValue) {
     return previousValue + (currentValue % 3 == 0 || currentValue % 5 == 0 ? currentValue : 0);
 });
 
-console.log(sumIfDivisibleBy3Or5)
+console.log(result)
