1056196ac108ad92dfc0685233a2ece9f6b1df76
Andy Stanton <andystanton@gmail.com>
2014-11-24 09:26:49 +0000

    add scala solution

---
+++ 2/scala/two.scala
@@ -0,0 +1,16 @@
+import annotation.tailrec
+
+def fibs(max: Int): Seq[Int] = {
+  fibs(Seq(2, 1), max)
+}
+
+@tailrec def fibs(fibSeq: Seq[Int], max: Int): Seq[Int] = {
+  fibSeq.head + fibSeq.tail.head match {
+    case valid: Int if valid < max => fibs(Seq(valid) ++ fibSeq, max)
+    case _ => fibSeq.reverse
+  }
+}
+
+val fibsTo50 = fibs(4000000)
+
+println(fibsTo50.filter(_ % 2 == 0).sum)
