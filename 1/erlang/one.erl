37cc51e3529792229042293b0929e943df848efb
Andy Stanton <andystanton@gmail.com>
2014-11-24 17:50:27 +0000

    improve haskell solution

--- 1/erlang/one.erl
+++ 1/erlang/one.erl
@@ -1,17 +1,15 @@
 -module(one).
--export([main/0, sum/1, sum/2, mod/2, addIfDivisibleBy3or5/2]).
-
-main() ->
-    io:format("~p\n", [sum(lists:seq(1,999))]).
-
-sum(L) -> sum(L,0).
-sum([], Sum) -> Sum;
-sum([H|T], Sum) -> sum(T, addIfDivisibleBy3or5(Sum, H)).
+-export([main/0, sum/1, sum/2, mod/2]).
 
 mod(X,Y)->(X rem Y + Y) rem Y.
+sum(L) -> sum(L,0).
+sum([], Sum) -> Sum;
+sum([H|T], Sum) -> sum(T, H+Sum).
 
-addIfDivisibleBy3or5(Sum, H) ->
-    case (mod(H, 3) == 0) or (mod(H, 5) == 0) of
-        true -> H+Sum;
-        _Else -> Sum
-    end.
+main() ->
+    io:format("~p\n", [sum(
+        lists:filter(
+            fun (X) -> (mod(X, 3) == 0) or (mod(X, 5) == 0) end,
+            lists:seq(1,999)
+        )
+    )]).
