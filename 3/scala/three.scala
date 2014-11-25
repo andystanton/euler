b606e1de1a85c11aa443978c9c7dac07f0f3aab6
Andy Stanton <andystanton@gmail.com>
2014-11-25 10:28:18 +0000

    add third problem and scala solution

---
+++ 3/scala/three.scala
@@ -0,0 +1,18 @@
+val target: Long = 600851475143L
+
+def sundaramSieve(max: Long): Seq[Long] = {
+  val range = (2L to max)
+  range.diff(for {
+    i <- range
+    j <- for {
+      other <- range
+    } yield other
+    candidate = i + j + 2 * i * j if (1 <= i) && (i <= j) && (candidate <= max)
+  } yield candidate).map(_ * 2 + 1)
+}
+
+val primes = sundaramSieve(10000L)
+val primeFactors = primes.filter(target % _ == 0)
+val largestPrime = primeFactors.reverse.head
+
+println(largestPrime)
