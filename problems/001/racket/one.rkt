109f67b266fdba3915e69ad9d16d543559055721
andystanton <andystanton@users.noreply.github.com>
2015-03-26 23:12:38 +0000

    solve problem 1 in racket

--- problems/001/racket/one.rkt
+++ problems/001/racket/one.rkt
@@ -1,16 +1,13 @@
 #lang racket
 
-;(define sum-list (list)
-;    (if list
-;        (+ (car list) (sum-list (cdr list)))
-;        0))
-;
-;(define rangeDivisibleBy3or5 (min max)
-;    (loop for n from min below max
-;        when (or (= (mod n 3) 0) (= (mod n 5) 0))
-;        collect n))
-;
-;(write (sum-list (rangeDivisibleBy3or5 1 1000)))
+(define (generate-range min max)
+    (build-list (- max min) (lambda (x) (+ min x))))
 
+(define (range-divisible-by-3-or-5 range)
+    (filter (lambda (x) (or (= (modulo x 3) 0) (= (modulo x 5) 0))) range))
 
-(write 233168)
+(define (sum-list list)
+    (foldl + 0 list))
+
+(write (sum-list (range-divisible-by-3-or-5 (generate-range 1 1000))))
+
